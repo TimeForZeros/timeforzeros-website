bb793a6ffbb6bfc255a0a78ff946cfdefa2fc957
Victor Gonzalez <dazedbutamused@gmail.com>
2020-10-03 23:35:11 -0700

    eslint setup

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,23 +3,18 @@ module.exports = {
     browser: true,
     commonjs: true,
     es6: true,
+    node: true,
   },
   extends: [
-    'airbnb',
+    'airbnb-base',
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 2018,
   },
-  plugins: [
-    'react',
-  ],
   rules: {
   },
 };
